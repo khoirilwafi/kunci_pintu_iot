86d638195c4905d8f237691b8f47cef3d034819b
khoirilwafi <68954908+khoirilwafi@users.noreply.github.com>
2023-03-22 17:13:10 +0700

    commit door component

---
+++ app/Http/Livewire/DoorComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class DoorComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.door-component');
+    }
+}
